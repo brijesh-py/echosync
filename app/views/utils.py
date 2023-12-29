8a977a94d846c8f523a9b486cc74d9abe1586f90
brijesh-py <bk6500416@gmail.com>
2023-12-29 13:29:18 +0530

    add icons

--- app/views/utils.py
+++ app/views/utils.py
@@ -6,19 +6,56 @@ from app import db
 
 class Utils:
     def get_file_icon(self, file):
-        file_extension = os.path.splitext(file)[1].lower()
-        if file_extension == ".pdf":
-            return "pdf.png"
-        if "image" in file:
-            return "image.png"
-        if "application" in file:
-            return "mobile-application.png"
-        if "text" in file:
+        try:
+            file_types = {
+                "pdf": "pdf.png",
+                "jpg": "image.png",
+                "jpeg": "image.png",
+                "png": "image.png",
+                "svg": "image.png",
+                "gif": "image.png",
+                "txt": "file.png",
+                "doc": "document.png",
+                "docx": "document.png",
+                "xls": "spreadsheet.png",
+                "xlsx": "spreadsheet.png",
+                "ppt": "presentation.png",
+                "pptx": "presentation.png",
+                "mp3": "audio.gif",
+                "wav": "audio.gif",
+                "mp4": "video.png",
+                "avi": "video.png",
+                "zip": "zip.png",
+                "rar": "zip.png",
+                "gz": "zip.png",
+                "exe": "exe.png",
+                "deb": "package.png",
+                "rpm": "package.png",
+                "py": "python.png",
+                "html": "html.png",
+                "css": "css.png",
+                "db": "db.png",
+                "js": "js.png",
+                "php": "php.png",
+                "deb": "deb.png",
+                "img": "img.png",
+                "sh": "sh.png",
+                "bash": "sh.png",
+                "zsh": "sh.png",
+                "json": "code.png",
+                "xml": "code.png",
+                "sql": "db.png",
+                "sqlite": "db.png",
+                "log": "log.png",
+                "xz": "tar.png",
+                "gz": "tar.png",
+                "md": "readme.png",
+                "gitignore": "git.png",
+                "application": "mobile-application.png",
+            }
+            return file_types[file]
+        except:
             return "file.png"
-        if "video" in file:
-            return "video.png"
-        else:
-            return "document.png"
 
     def get_file_size(self, file):
         bytes_size = os.path.getsize(file)
@@ -32,7 +69,7 @@ class Utils:
         return f"{bytes_size} Bytes"
 
     def upload_file_data(self, file, file_path):
-        file_extension = os.path.splitext(file.filename)[1].lower()
+        file_extension = os.path.splitext(file.filename)[1][1:].lower()
         file_icon = self.get_file_icon(file_extension)
 
         file_db = FilesDB(
